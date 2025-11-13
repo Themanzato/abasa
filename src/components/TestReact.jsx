577d726ee8ec1a3fa2b73c176c4abb5bbdf29874
Manzato <mb13arroyo@gmail.com>
2025-11-13 10:53:25 -0600

    Configuración de React y Tailwind

---
+++ src/components/TestReact.jsx
@@ -0,0 +1,8 @@
+export default function TestReact() {
+    return (
+      <div className="p-4 bg-blue-500 text-white rounded-lg">
+        <h2>Componente React funcionando</h2>
+      </div>
+    );
+  }
+  
